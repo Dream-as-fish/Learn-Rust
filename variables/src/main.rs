a24b89be536b13a52d5b759093696ce8e23b6632
夢為魚 <ywaterdog@outlook.com>
2024-02-15 14:25:17 +0800

    variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,12 +3,28 @@ fn main() {
     println!("The value of x is:{x}");
     x = 6;
     println!("The value of x is:{x}");*/
-    let x = 5;
+
+    /*let x = 5;
     let x = x + 1;
     {
         let x = x * 2;
         print!("The value of x in the inner scope is:{x}\n");
     }
 
-    println!("The value of x is:{x}\n");
+    println!("The value of x is:{x}\n");*/
+
+    /*let tup = (500, 6.4, 1);
+
+    let (x, y, z) = tup;
+
+    println!("The value of y is:{y}")*/
+
+    let a = [1, 2, 3, 4, 5];
+
+    let first = a[0];
+    let second = a[1];
+
+    let b: [i32; 5] = [1, 2, 3, 4, 5];
+
+    let c = [3; 5];
 }
